8d71e69c5eea97792198bce38d503360fbd89dab
__xduong <48197320+nb-duong@users.noreply.github.com>
2022-11-20 14:08:17 +0700

    Update vm_whatsUpIndexes.sql

--- vm_whatsUpIndexes.sql
+++ vm_whatsUpIndexes.sql
@@ -1,18 +1,4 @@
-
-IF OBJECT_ID('dbo.WhatsUpIndexes') IS NULL
-BEGIN
-    DECLARE @vsql nvarchar(MAX) = N'
-    CREATE VIEW dbo.WhatsUpIndexes
-    AS
-    	SELECT x = 1;';
-    
-		PRINT @vsql;
-		EXEC (@vsql);
-	END;
-GO 
-
-
-ALTER VIEW dbo.WhatsUpIndexes
+CREATE OR ALTER VIEW dbo.WhatsUpIndexes
 AS
 	SELECT TOP (2147483647)
 		view_name = 'WhatsUpIndexes',
@@ -28,5 +14,5 @@ AS
 	JOIN sys.objects AS so ON  ps.object_id = so.object_id AND so.is_ms_shipped = 0 AND so.type <> 'TF'
 	JOIN sys.schemas AS s ON s.schema_id = so.schema_id
 	JOIN sys.indexes AS i ON  ps.object_id = i.object_id AND ps.index_id  = i.index_id
-	ORDER BY ps.object_id, ps.index_id, ps.partition_number;
-GO
+	ORDER BY ps.object_id, ps.index_id, ps.partition_number
+;
